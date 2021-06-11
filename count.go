039f0dd72482dcdc18a8eab5fb3565d86ad50e8a
Vimal Karthik <vimalkarthik@apstrix.com>
2021-06-11 10:35:02 +0800

    print anniversary

---
+++ count.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func count() {
+
+	for i := 11; i < 21; i++ {
+		fmt.Printf(" Starting from yr 20%d\n", i)
+	}
+}
--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 
 	fmt.Println("Micro Services 3")
+
 }
